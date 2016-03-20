2d741c710a135728c2028cac9cec3dd0fbda80bd
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-20 11:09:32 -0400

    Update Flexival.jl

--- src/type/Flexival.jl
+++ src/type/Flexival.jl
@@ -1,1 +1,13 @@
 
+
+         #                 type covering   configures
+         #                 value-context   boundaries
+
+abstract AbstractInterval{    Surface,      Tension                }   <: Number
+
+         #                 type covering   configures    perceptual
+         #                 value-context   boundaries    values map
+
+immutable FlexibleInterval{   Surface,      Tension,       Action   } <: AnyInterval{ Surface, Tension }
+
+
