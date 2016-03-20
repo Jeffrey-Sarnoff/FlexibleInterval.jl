cb291db6b1d2c0eab5615e8ac00a70c8eef7ea31
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-20 15:57:57 -0400

    Update Flexival.jl

--- src/type/Flexival.jl
+++ src/type/Flexival.jl
@@ -10,23 +10,6 @@ abstract  OneBoundary          <: AnyBoundaries
 abstract  TwoBoundaries        <: AnyBoundaries
 abstract  ThreeBoundaries      <: AnyBoundaries
 
-immutable  OpeneD              <: OneBoundary     end; Opened             = OpeneD()
-immutable  CloseD              <: OneBoundary     end; Closed             = CloseD()
-
-immutable  OpenedOpeneD        <: TwoBoundaries   end; OpenedOpened       = OpenedOpeneD()      
-immutable  OpenedCloseD        <: TwoBoundaries   end; OpenedClosed       = OpenedCloseD()      
-immutable  ClosedOpeneD        <: TwoBoundaries   end; ClosedOpened       = ClosedOpeneD()      
-immutable  ClosedCloseD        <: TwoBoundaries   end; ClosedClosed       = ClosedCloseD()      
-
-immutable  OpenedOpenedOpeneD  <: ThreeBoundaries end; OpenedOpenedOpened = OpenedOpenedOpeneD()
-immutable  OpenedOpenedCloseD  <: ThreeBoundaries end; OpenedOpenedClosed = OpenedOpenedCloseD()
-immutable  OpenedClosedOpeneD  <: ThreeBoundaries end; OpenedClosedOpened = OpenedClosedOpeneD()
-immutable  OpenedClosedCloseD  <: ThreeBoundaries end; OpenedClosedClosed = OpenedClosedCloseD()
-immutable  ClosedOpenedOpeneD  <: ThreeBoundaries end; ClosedOpenedOpened = ClosedOpenedOpeneD()
-immutable  ClosedOpenedCloseD  <: ThreeBoundaries end; ClosedOpenedClosed = ClosedOpenedCloseD()
-immutable  ClosedClosedOpeneD  <: ThreeBoundaries end; ClosedClosedOpened = ClosedClosedOpeneD()
-immutable  ClosedClosedCloseD  <: ThreeBoundaries end; ClosedClosedClosed = ClosedClosedCloseD()
-
 
 
 abstract          AbsInterval{     Surface,       Tension                  } <: Number
@@ -46,6 +29,26 @@ immutable FlexibleInterval{ Surface <: Real,
     FlexibleInterval{S}(lo::S,hi::S) = (lo <= hi) ? new(lo,hi) : new(hi,lo)
 end
 
+
+
+
+
+immutable  OpeneD              <: OneBoundary     end; Opened             = OpeneD()
+immutable  CloseD              <: OneBoundary     end; Closed             = CloseD()
+
+immutable  OpenedOpeneD        <: TwoBoundaries   end; OpenedOpened       = OpenedOpeneD()      
+immutable  OpenedCloseD        <: TwoBoundaries   end; OpenedClosed       = OpenedCloseD()      
+immutable  ClosedOpeneD        <: TwoBoundaries   end; ClosedOpened       = ClosedOpeneD()      
+immutable  ClosedCloseD        <: TwoBoundaries   end; ClosedClosed       = ClosedCloseD()      
+
+immutable  OpenedOpenedOpeneD  <: ThreeBoundaries end; OpenedOpenedOpened = OpenedOpenedOpeneD()
+immutable  OpenedOpenedCloseD  <: ThreeBoundaries end; OpenedOpenedClosed = OpenedOpenedCloseD()
+immutable  OpenedClosedOpeneD  <: ThreeBoundaries end; OpenedClosedOpened = OpenedClosedOpeneD()
+immutable  OpenedClosedCloseD  <: ThreeBoundaries end; OpenedClosedClosed = OpenedClosedCloseD()
+immutable  ClosedOpenedOpeneD  <: ThreeBoundaries end; ClosedOpenedOpened = ClosedOpenedOpeneD()
+immutable  ClosedOpenedCloseD  <: ThreeBoundaries end; ClosedOpenedClosed = ClosedOpenedCloseD()
+immutable  ClosedClosedOpeneD  <: ThreeBoundaries end; ClosedClosedOpened = ClosedClosedOpeneD()
+immutable  ClosedClosedCloseD  <: ThreeBoundaries end; ClosedClosedClosed = ClosedClosedCloseD()
 
 
 
