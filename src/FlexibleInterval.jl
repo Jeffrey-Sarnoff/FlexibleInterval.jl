1976b9eafae88fe2b0aa44a5079bdc6ad88b757a
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-20 10:12:22 -0400

    Update FlexibleInterval.jl

--- src/FlexibleInterval.jl
+++ src/FlexibleInterval.jl
@@ -1,4 +1,34 @@
 module FlexibleInterval.jl
 
+import Base: hash, convert, promote_rule,
+    string, show, showcompact, showall,
+    isnan, isinf, isfinite, issubnormal,
+    sign, signbit, copysign, flipsign,
+    ispow2, prevpow2, nextpow2, frexp, ldexp,
+    abs, sqrt, abs2, hypot,
+    isequal, isless, 
+    (==), (!=), (<), (<=), (>=), (>),
+    (+),(-),(*),(/),(\),(%),(^),(//),
+    log, log1p, log2, log10, exp, expm1,
+    sin, cos, tan, csc, sec, cot,
+    sinh, cosh, tanh, csch, sech, coth,
+    asin, acos, atan, acsc, asec, acot, atan2,
+    asinh, acosh, atanh, acsch, asech, acoth
+    
+    
+    
+    
+    
+export show, showcompact, showall,  # sometimes necessary (q.v. Nemo/arb)
+    ispos, isneg, isnormal, ntneg, ntnan, ntnormal,
+    ispow2, prevpow2, nextpow2, frexp, ldexp,
+    abs, sqrt, abs2, hypot,
+    sqr
+
+
+include("type/Flexival.jl")
+include("type/io.jl")
+include("type/generous.jl")
+
 
 end
