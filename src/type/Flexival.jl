1d610b43c91cdb4700ca51a4de0ac99fcba2eae7
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-20 12:04:26 -0400

    Update Flexival.jl

--- src/type/Flexival.jl
+++ src/type/Flexival.jl
@@ -1,14 +1,57 @@
+MaybeFunction = Nullable{Function}
+nullFunction  = MaybeFunction()
+typealias       maybeFunction Union{Function, MaybeFunction}
+
+Base.isnull(x::Function) = false
 
 
-abstract       AnyInterval{     Surface,       Tension                      } <: Number
+abstract AnyBoundaries
+abstract  OneBoundary          <: AnyBoundaries
+abstract  TwoBoundaries        <: AnyBoundaries
+abstract  ThreeBoundaries      <: AnyBoundaries
 
-        #                     type covering   configures     perceptual
-        #                     value-context   boundaries     values map
+abstract   Opened              <: OneBoundary
+abstract   Closed              <: OneBoundary
 
-abstract       AnyFlexival{     Surface,       Tension,        Action       } <: AnyInterval{ Surface, Tension }
+abstract   OpenedOpened        <: TwoBoundaries
+abstract   OpenedClosed        <: TwoBoundaries
+abstract   ClosedOpened        <: TwoBoundaries
+abstract   ClosedClosed        <: TwoBoundaries
+
+abstract   OpenedOpenedOpened  <: ThreeBoundaries
+abstract   OpenedOpenedClosed  <: ThreeBoundaries
+abstract   OpenedClosedOpened  <: ThreeBoundaries
+abstract   OpenedClosedClosed  <: ThreeBoundaries
+abstract   ClosedOpenedOpened  <: ThreeBoundaries
+abstract   ClosedOpenedClosed  <: ThreeBoundaries
+abstract   ClosedClosedOpened  <: ThreeBoundaries
+abstract   ClosedClosedClosed  <: ThreeBoundaries
 
 
-immutable FlexibleInterval{ Surface<:Real, Tension<:Tuple, Action:<Function } <: AnyFlexival{Surface,Tension,Action}
+
+abstract          AbsInterval{     Surface,       Tension                      } <: Number
+
+                             #   type covering   configures     perceptual
+                             #   value-context   boundaries     values map
+
+abstract  AbsFlexibleInterval{     Surface,       Tension,        Action       } <: AbsInterval{ Surface, Tension }
+
+
+immutable FlexibleInterval{ Surface <: Real, 
+                            Tension <: Tuple, 
+                            Action  <: MaybeFunction } <: AbsFlexibleInterval{Surface,Tension,Action}
+    lo::Surface
+    hi::Surface
+    
+    FlexibleInterval{S}(lo::S,hi::S) = (lo <= hi) ? new(lo,hi) : new(hi,lo)
+end
+
+
+
+FlexibleInterval{S,T,A}(::Type{T}, ::Type{A}, lo::S, hi::S) = FlexibleInterval{S,T,A}(lo,hi)
+FlexibleInterval{S,T}(::Type{T}, lo::S, hi::S) = FlexibleInterval{S,T,A}(T, nullFunction, lo, hi)
+FlexibleInterval{S,A}(::Type{A}, lo::S, hi::S) = FlexibleInterval{S,T,A}(T, nullFunction, lo, hi)
+
 
 
 
